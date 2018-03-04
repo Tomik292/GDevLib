98950744b302a46c82b47b71d3a17ab14d1e95a2
Tomáš Houfek <tomashoufek4@seznam.cz>
2018-03-04 10:31:37 +0100

    registration, login and logout

--- GDevLibrary/app/forms.py
+++ GDevLibrary/app/forms.py
@@ -1,4 +1,30 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from crispy_forms.helper import FormHelper
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+    re_password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField();
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        re_password = self.cleaned_data['re_password']
+        email = self.cleaned_data['email']
+
+        if len(password) < 8:
+            raise forms.ValidationError("Password needs to be atleast 8 characters long!")
+
+        if password != re_password:
+            raise forms.ValidationError("Passwords aren't matching!")
+
+class LoginForm(forms.Form):
+    name = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
--- GDevLibrary/app/views.py
+++ GDevLibrary/app/views.py
@@ -1,36 +1,103 @@
-
-from django.shortcuts import render
-from django.http import HttpRequest
-from django.template import RequestContext
 from datetime import datetime
 
-def login(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/login.html')
+from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
+from django.core.mail import send_mail
+
+from .forms import UserForm, LoginForm
+
+
+def UserRegisterView(request):
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
+    template_name = 'app/register.html'
+    if request.method == 'POST': 
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False) 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user.set_password(password)
+            user.save()
+
+            send_mail(
+             'Welcome to GDevLibrary',
+             'Click on this lick for your email authetizatation',
+             settings.EMAIL_HOST_USER,
+              [email, settings.EMAIL_HOST_USER],
+              fail_silently = False
+             )
+
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render(request, template_name, {'form':form})
+
+def UserLoginView(request):
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
+    template_name = 'app/login.html'
+    if request.method ==  'POST':
+        form =  LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["name"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(username = username, password = password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('home')
+    else:
+        form = LoginForm()
+    
+    content= {
+        'form':form,
+        'user':user 
+        }
+
+    return render(request, template_name, content)
+
+
 
 def home(request):
     """Renders the home page."""
-    assert isinstance(request, HttpRequest)
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
     return render(
         request,
-        'app/main.html')
+        'app/main.html',
+        {'user':user})
 
 def main_unity(request):
-    assert isinstance(request, HttpRequest)
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
     return render(
         request,
-       'app/unity_main.html')
+       'app/unity_main.html',
+       {'user':user})
 
 def main_unreal(request):
-    assert isinstance(request, HttpRequest)
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
     return render(
         request,
-        'app/unreal_main.html')
+        'app/unreal_main.html',
+        {'user':user})
 
 def main_cry(request):
-    assert isinstance(request, HttpRequest)
+    user = request.user if request.user.is_authenticated() else None
+    print(user)
     return render(
         request,
-       'app/cry_main.html')
+       'app/cry_main.html',
+       {'user':user})
