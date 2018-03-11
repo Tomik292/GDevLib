479913081e940b3c5954cb0a59ade66ea6c01e77
Tomáš Houfek <tomashoufek4@seznam.cz>
2018-03-11 21:16:50 +0100

    User, category and msg models

--- GDevLibrary/app/models.py
+++ GDevLibrary/app/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.CharField(max_length=50)
+    sender = models.ForeignKey(User, on_delete=models.PROTECT)
     recipient = models.CharField(max_length=50)
     text = models.TextField(max_length=500)
     sedingTime = models.DateTimeField()
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    engine = models.CharField(max_length=50)
 
-    
+class UserExtension(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField()
+    bio = models.TextField(max_length=500)
+    favorite_categories = models.ManyToManyField(Category)
+    location = models.CharField(max_length=50)
+
+
+
     
