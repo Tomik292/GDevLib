05252459fe91ff553cb1e64c9b308e435b96e89a
Tomáš Houfek <tomashoufek4@seznam.cz>
2018-04-02 19:41:51 +0200

    Main pages, user page, user forms, search, engines pages

--- GDevLibrary/app/forms.py
+++ GDevLibrary/app/forms.py
@@ -27,15 +27,44 @@ class UserForm(forms.ModelForm):
         if password != re_password:
             raise forms.ValidationError("Passwords aren't matching!")
 
-class UserExtensionForm(forms.ModelForm):
+class UserExtensionForm(forms.Form):
 
-    class Meta:
-        model = UserExtension
-        fields = ['picture','bio','location']
+    username = forms.CharField(max_length=150, required=False)
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=150, required=False)
+    email =  forms.EmailField(required=False)
+    bio = forms.CharField(widget=forms.Textarea, max_length=500, required=False)
+    location = forms.CharField(max_length=50, required=False)
+    new_password = forms.CharField(widget=forms.PasswordInput, required=False)
+    new_password_again = forms.CharField(widget=forms.PasswordInput, required=False)
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        email = self.cleaned_data['email']
+        bio = self.cleaned_data['bio']
+        location = self.cleaned_data['location']
+        new_password = self.cleaned_data['new_password']
+        new_password_again = self.cleaned_data['new_password_again']
+
+        if User.objects.filter(username = username).exists():
+            raise forms.ValidationError("User with this name already exists")
+
+
+
 
 class LoginForm(forms.Form):
     name = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        name = self.cleaned_data['name']
+        password = self.cleaned_data['password']
+
+        if not User.objects.filter(username = name).exists():
+            raise forms.ValidationError("User doesn't exist!")
+        
 
 class MessageForm(forms.ModelForm):
 
@@ -47,4 +76,7 @@ class ArticleForm(forms.ModelForm):
 
     class Meta:
         model = Article
-        fields = ['name','picture','engine','text','tags']
+        fields = ['name','picture','engine','text','overview']
+
+class SearchForm(forms.Form):
+    key_words = forms.CharField(max_length = 200, label="", widget = forms.TextInput(attrs={'placeholder':'Search'}))
